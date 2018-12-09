9a18a3dbd1a7a7f644efd1b7af46583bf1e5daaa
wangxinshuo <6636396@gmail.com>
2018-12-09 21:40:07 +0800

    .

---
+++ 智能社React第二讲 课件/react_webpack/index.js
@@ -0,0 +1,20 @@
+const React=require('React');
+
+class Test extends React.Component{
+  constructor(...args){
+    super(...args);
+
+    this.state={value: ''};
+  }
+  fn(){
+    this.setState({
+      value: this.refs['txt1'].value
+    });
+  }
+  render(){
+    return <div>
+      <input ref="txt1" type="text" onChange={this.fn.bind(this)} />
+      <span>{this.state.value}</span>
+    </div>;
+  }
+}
